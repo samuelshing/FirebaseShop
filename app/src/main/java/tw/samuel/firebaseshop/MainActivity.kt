57abff7d07050f5db12b96a6017054fa8da1dff3
Samuel Shing <samuelshing@gmail.com>
2019-04-24 09:22:33 +0800

    tab

--- app/src/main/java/tw/samuel/firebaseshop/MainActivity.kt
+++ app/src/main/java/tw/samuel/firebaseshop/MainActivity.kt
@@ -15,71 +15,71 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
-    private val TAG = MainActivity::class.java.simpleName
-    private val RC_SIGNIN: Int = 100
+	private val TAG = MainActivity::class.java.simpleName
+	private val RC_SIGNIN: Int = 100
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        verify_email.setOnClickListener {
-            FirebaseAuth.getInstance().currentUser?.sendEmailVerification()
-                ?.addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Snackbar.make(it, "Verify email sent", Snackbar.LENGTH_LONG).show()
-                    }
-                }
-        }
-    }
+		fab.setOnClickListener { view ->
+			Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+					.setAction("Action", null).show()
+		}
+		verify_email.setOnClickListener {
+			FirebaseAuth.getInstance().currentUser?.sendEmailVerification()
+					?.addOnCompleteListener { task ->
+						if (task.isSuccessful) {
+							Snackbar.make(it, "Verify email sent", Snackbar.LENGTH_LONG).show()
+						}
+					}
+		}
+	}
 
-    override fun onStart() {
-        super.onStart()
-        FirebaseAuth.getInstance().addAuthStateListener(this)
-    }
+	override fun onStart() {
+		super.onStart()
+		FirebaseAuth.getInstance().addAuthStateListener(this)
+	}
 
-    override fun onStop() {
-        super.onStop()
-        FirebaseAuth.getInstance().removeAuthStateListener(this)
-    }
+	override fun onStop() {
+		super.onStop()
+		FirebaseAuth.getInstance().removeAuthStateListener(this)
+	}
 
-    override fun onAuthStateChanged(auth: FirebaseAuth) {
-        val user = auth.currentUser
-        Log.d(TAG, "onAuthStateChanged: ${user?.uid}")
-        if (user != null) {
-            user_info.setText("Email: ${user.email} / ${user.isEmailVerified}")
-            verify_email.visibility = if (user.isEmailVerified) View.GONE else View.VISIBLE
-        } else {
-            user_info.setText("Not login")
-            verify_email.visibility = View.GONE
-        }
-    }
+	override fun onAuthStateChanged(auth: FirebaseAuth) {
+		val user = auth.currentUser
+		Log.d(TAG, "onAuthStateChanged: ${user?.uid}")
+		if (user != null) {
+			user_info.setText("Email: ${user.email} / ${user.isEmailVerified}")
+			verify_email.visibility = if (user.isEmailVerified) View.GONE else View.VISIBLE
+		} else {
+			user_info.setText("Not login")
+			verify_email.visibility = View.GONE
+		}
+	}
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		menuInflater.inflate(R.menu.menu_main, menu)
+		return true
+	}
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            R.id.action_signin -> {
-                startActivityForResult(Intent(this, SignInActivity::class.java), RC_SIGNIN)
-                true
-            }
-            R.id.action_signout -> {
-                FirebaseAuth.getInstance().signOut()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		return when (item.itemId) {
+			R.id.action_settings -> true
+			R.id.action_signin -> {
+				startActivityForResult(Intent(this, SignInActivity::class.java), RC_SIGNIN)
+				true
+			}
+			R.id.action_signout -> {
+				FirebaseAuth.getInstance().signOut()
+				true
+			}
+			else -> super.onOptionsItemSelected(item)
+		}
+	}
 }
--- app/src/main/java/tw/samuel/firebaseshop/SignInActivity.kt
+++ app/src/main/java/tw/samuel/firebaseshop/SignInActivity.kt
@@ -9,55 +9,55 @@ import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
-        signUp.setOnClickListener {
-            signUp()
-        }
-        login.setOnClickListener {
-            login()
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_sign_in)
+		signUp.setOnClickListener {
+			signUp()
+		}
+		login.setOnClickListener {
+			login()
+		}
+	}
 
-    private fun login() {
-        val sEmail = email.text.toString()
-        val sPassword = password.text.toString()
-        FirebaseAuth.getInstance()
-            .signInWithEmailAndPassword(sEmail, sPassword)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    setResult(Activity.RESULT_OK)
-                    finish()
-                } else {
-                    AlertDialog.Builder(this@SignInActivity)
-                        .setTitle("Login")
-                        .setMessage(task.exception?.message)
-                        .setPositiveButton("OK", null).show()
-                }
-            }
-    }
+	private fun login() {
+		val sEmail = email.text.toString()
+		val sPassword = password.text.toString()
+		FirebaseAuth.getInstance()
+				.signInWithEmailAndPassword(sEmail, sPassword)
+				.addOnCompleteListener { task ->
+					if (task.isSuccessful) {
+						setResult(Activity.RESULT_OK)
+						finish()
+					} else {
+						AlertDialog.Builder(this@SignInActivity)
+								.setTitle("Login")
+								.setMessage(task.exception?.message)
+								.setPositiveButton("OK", null).show()
+					}
+				}
+	}
 
-    private fun signUp() {
-        val sEmail = email.text.toString()
-        val sPassword = password.text.toString()
-        FirebaseAuth.getInstance()
-            .createUserWithEmailAndPassword(sEmail, sPassword)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    AlertDialog.Builder(this@SignInActivity)
-                        .setTitle("Sign In")
-                        .setMessage("Account create")
-                        .setPositiveButton("OK") { dialog, which ->
-                            setResult(Activity.RESULT_OK)
-                            finish()
-                        }.show()
-                } else {
-                    AlertDialog.Builder(this@SignInActivity)
-                        .setTitle("Sign Up")
-                        .setMessage(task.exception?.message)
-                        .setPositiveButton("OK", null).show()
-                }
-            }
-    }
+	private fun signUp() {
+		val sEmail = email.text.toString()
+		val sPassword = password.text.toString()
+		FirebaseAuth.getInstance()
+				.createUserWithEmailAndPassword(sEmail, sPassword)
+				.addOnCompleteListener { task ->
+					if (task.isSuccessful) {
+						AlertDialog.Builder(this@SignInActivity)
+								.setTitle("Sign In")
+								.setMessage("Account create")
+								.setPositiveButton("OK") { dialog, which ->
+									setResult(Activity.RESULT_OK)
+									finish()
+								}.show()
+					} else {
+						AlertDialog.Builder(this@SignInActivity)
+								.setTitle("Sign Up")
+								.setMessage(task.exception?.message)
+								.setPositiveButton("OK", null).show()
+					}
+				}
+	}
 }
